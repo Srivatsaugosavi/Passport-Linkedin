20a63f4cb7d3ff99c59debafe3a6f232a004e6d8
Srivatsaugosavi <92358096+Srivatsaugosavi@users.noreply.github.com>
2022-01-20 14:20:51 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+modules.exports={
+	'LinkedinAuth':{
+	'clientID':'86yubwdpnz4b73',//user id
+	'clientSecret':'7WzzwGA9AsArJqRJ',//your app secret
+	'callbackURL':'http://127.0.0.1:3000/auth/linkedin/callback'
+	}
+}
